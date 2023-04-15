92115a6ca1a6ff9b3fad0803a506990c0488a7cc
hugh-eu <hugh_eu@outlook.com>
2023-04-15 21:10:01 +0900

    20230415_2109

--- react_redux_practice/todos/src/Title.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-export default class TitleBar extends Component {
-    render() {
-        return (
-            <div>{this.props.text}</div>
-        );
-    }
-}
--- react_redux_practice/todos/src/TodoList.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-export default class TodoList extends Component {
-    render() {
-        return (
-            <ul>{this.props.items}</ul>
-        );
-    }
-}
